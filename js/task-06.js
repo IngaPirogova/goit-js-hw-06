88b686dce5d3081d42a41c6e60ff36cec97fb09c
IngaPirogova <ingeborga.pirogova@gmail.com>
2022-10-03 23:38:10 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -7,15 +7,19 @@ inputText.addEventListener('blur', onInputValidation);
 
 function onInputValidation(event) {
 
-    if (Number(event.currentTarget.value.length) === Number(inputLength)) {
-             inputText.classList.add("valid");
-    } else {
-        inputText.classList.add("invalid");
-    }  
-};
-       
- 
+        if (event.currentTarget.value.length == inputLength) {
+            inputText.classList.add("valid");
+            inputText.classList.remove("invalid");
+        } else {
+            inputText.classList.add("invalid");
+            inputText.classList.remove("valid");
+        }  
+    };
 
+
+
+    
+ 
 
 
 
