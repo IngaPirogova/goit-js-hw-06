54d330cf3603af71f5410396bf671b9ecec52226
IngaPirogova <ingeborga.pirogova@gmail.com>
2022-11-17 18:17:41 +0300

    summary

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,43 @@
 // function getRandomHexColor() {
 //   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 // }
+const inputNumber = document.querySelector('input');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const divBoxes = document.querySelector('#boxes');
+const divControls= document.querySelector('#controls');
+const startValuePx = 30;
+
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function createBoxes(amount) {
+    const divElAdd = [];
+    for (let i = 0; i < amount; i++) {
+        const divElCreate = document.createElement('div');
+        divElCreate.style.width = `${startValuePx + 10 * i}px`
+        divElCreate.style.height = `${startValuePx + 10 * i}px`
+        divElCreate.style.background = getRandomHexColor();
+        
+        divElAdd.push(divElCreate);
+    }
+     return divElAdd;   
+};
+
+ function destroyBoxes() {
+     divBoxes.innerHTML = '';
+}
+
+btnCreate.addEventListener('click', () => {
+    let boxesToAdd = createBoxes(inputNumber.value)
+    divBoxes.append(...boxesToAdd)
+
+     console.log(divBoxes)
+});
+
+btnDestroy.addEventListener('click', () => {
+    destroyBoxes.call()
+});
